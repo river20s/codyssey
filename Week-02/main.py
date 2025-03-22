dea4e52846b6b740f59842446ab8f06bc2ade051
river20s <river20s@naver.com>
2025-03-23 01:32:09 +0900

    feat: 로그 파일 읽기 방식 개선 및 시간 역순 출력 추가
    read() 대신 readline()을 사용하도록 개선했다.
    #2

--- Week-02/main.py
+++ Week-02/main.py
@@ -7,18 +7,24 @@ def main():
     try:
         # 로그 파일을 열고 읽음
         with open(log_path, encoding = 'utf-8') as log_file:
-            log_content = log_file.read()
             # 로그 파일이 열렸음을 알리는 메시지 출력
-            print(f"파일 열림: {log_path}\n")
-            # 보너스 과제 수행을 위해 로그 파일 내용을 시간 역순으로 출력
-            log_lines = log_content.splitlines()
-            header = log_lines[0] # 첫 번째 줄은 헤더로 저장
-            log_lines = log_lines[1:] # 헤더를 제외한 나머지 줄
-            reversed_log_content = "\n".join(log_lines[::-1])
+            print(f'파일 열림: {log_path}\n')
 
+            # 첫 번째 줄은 헤더로 저장
+            header = log_file.readline().rstrip('\n') 
+            # 나머지 줄 리스트에 저장
+            log_lines = []
+            while True:
+                line = log_file.readline()
+                if not line:
+                    break
+                log_lines.append(line.rstrip('\n'))
             # 상위 3개의 문제가 되는 로그를 추출함
             major_issues_lines = log_lines[:3]
-            major_issues_content = "\n".join(major_issues_lines)
+            major_issues_content = '\n'.join(major_issues_lines)
+
+            # 로그 내용 시간 역순 출력
+            reversed_log_content = '\n'.join(log_lines[::-1])
 
             print(header) # 헤더 출력
             print(reversed_log_content) # 시간 역순으로 로그 내용 출력
